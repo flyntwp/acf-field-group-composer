2111e9f1cc7343fa213802499435219cb13bcba3
Dominik Tränklein <dominik@bleech.de>
2016-11-07 17:49:29 +0100

    chore(setup): initial commit

---
+++ tests/TestCase.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ACFComposer;
+
+use PHPUnit\Framework;
+use Brain\Monkey;
+
+class TestCase extends Framework\TestCase {
+
+  protected function setUp() {
+    parent::setUp();
+    Monkey::setUpWP();
+  }
+
+  protected function tearDown() {
+    Monkey::tearDownWP();
+    parent::tearDown();
+  }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package ACF_Field_Group_Composer
+ */
+
+
+// First we need to load the composer autoloader so we can use WP Mock
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+
+require_once __DIR__. '/TestCase.php';
