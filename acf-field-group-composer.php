548892de052451f578c399c18c1aca25d74af6d2
Dominik Tränklein <dominik@bleech.de>
2016-11-08 22:02:28 +0100

    fix(plugin): load correct plugin files

--- acf-field-group-composer.php
+++ acf-field-group-composer.php
@@ -12,6 +12,5 @@
  * @package         ACF_Field_Group_Composer
  */
 
-require_once __DIR__ . '/lib/WPStarter/Helpers.php';
-require_once __DIR__ . '/lib/WPStarter/ConstructionPlan.php';
-require_once __DIR__ . '/lib/WPStarter/Renderer.php';
+require_once __DIR__ . '/lib/ACFComposer/ResolveConfig.php';
+require_once __DIR__ . '/lib/ACFComposer/ACFComposer.php';
