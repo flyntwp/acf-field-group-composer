796b862297cc3afed870a42ae043f8d55c5aff81
Dominik Tränklein <dominik@bleech.de>
2016-11-08 16:09:18 +0100

    feat(layout): add basic layout class

---
+++ lib/ACFComposer/Layout.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ACFComposer;
+
+use Exception;
+
+class Layout {
+  function __construct($config) {
+    if(is_string($config)) {
+      $config = apply_filters($config, null);
+    }
+    $this->config = $this->validateConfig($config);
+  }
+
+  protected function validateConfig($config) {
+    if(!array_key_exists('name', $config)) {
+      throw new Exception('Field config needs to contain a \'name\' property.');
+    }
+    if(!array_key_exists('label', $config)) {
+      throw new Exception('Field config needs to contain a \'label\' property.');
+    }
+    if(array_key_exists('key', $config)) {
+      throw new Exception('Field config must not contain a \'key\' property.');
+    }
+    return $config;
+  }
+}
