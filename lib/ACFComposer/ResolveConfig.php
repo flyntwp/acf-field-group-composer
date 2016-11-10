158b1eb10a3b9b60d15653ad9828a4f5ffdeb30f
Dominik Tränklein <dominik@bleech.de>
2016-11-10 12:35:47 +0100

    style(general): fix phpcs errors

--- lib/ACFComposer/ResolveConfig.php
+++ lib/ACFComposer/ResolveConfig.php
@@ -10,7 +10,7 @@ class ResolveConfig {
 
     $keySuffix = $output['name'];
     $output['key'] = "group_{$keySuffix}";
-    $output['fields'] = array_map(function($field) use ($keySuffix){
+    $output['fields'] = array_map(function ($field) use ($keySuffix) {
       return self::forField($field, $keySuffix);
     }, $output['fields']);
     $output['location'] = array_map('self::mapLocation', $output['location']);
@@ -30,30 +30,31 @@ class ResolveConfig {
   }
 
   protected static function forEntity($config, $requiredAttributes, $parentKeySuffix = '') {
-    if(is_string($config)) {
+    if (is_string($config)) {
       $config = apply_filters($config, null);
     }
     $output = self::validateConfig($config, $requiredAttributes);
 
-    $keySuffix = empty($parentKeySuffix) ? $output['name'] : "{$parentKeySuffix}_{$output['name']}";
+    $keySuffix = empty($parentKeySuffix)
+      ? $output['name']
+      : "{$parentKeySuffix}_{$output['name']}";
     $output['key'] = "field_{$keySuffix}";
 
     $output = apply_filters('ACFComposer/resolveEntity', $output);
     $output = apply_filters("ACFComposer/resolveEntity?name={$output['name']}", $output);
     $output = apply_filters("ACFComposer/resolveEntity?key={$output['key']}", $output);
-
     $output = self::forNestedEntities($output, $keySuffix);
     return $output;
   }
 
   protected static function forNestedEntities($config, $parentKeySuffix) {
-    if(array_key_exists('sub_fields', $config)) {
-      $config['sub_fields'] = array_map(function($field) use ($parentKeySuffix){
+    if (array_key_exists('sub_fields', $config)) {
+      $config['sub_fields'] = array_map(function ($field) use ($parentKeySuffix) {
         return self::forField($field, $parentKeySuffix);
       }, $config['sub_fields']);
     }
-    if(array_key_exists('layouts', $config)) {
-      $config['layouts'] = array_map(function($layout) use ($parentKeySuffix){
+    if (array_key_exists('layouts', $config)) {
+      $config['layouts'] = array_map(function ($layout) use ($parentKeySuffix) {
         return self::forLayout($layout, $parentKeySuffix);
       }, $config['layouts']);
     }
@@ -61,12 +62,12 @@ class ResolveConfig {
   }
 
   protected static function validateConfig($config, $requiredAttributes = []) {
-    array_walk($requiredAttributes, function($key) use ($config){
-      if(!array_key_exists($key, $config)) {
+    array_walk($requiredAttributes, function ($key) use ($config) {
+      if (!array_key_exists($key, $config)) {
         throw new Exception("Field config needs to contain a \'{$key}\' property.");
       }
     });
-    if(array_key_exists('key', $config)) {
+    if (array_key_exists('key', $config)) {
       throw new Exception('Field config must not contain a \'key\' property.');
     }
     return $config;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,6 @@
 
 
 // First we need to load the composer autoloader so we can use WP Mock
-require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 require_once __DIR__. '/TestCase.php';
