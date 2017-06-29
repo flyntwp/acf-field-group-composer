024c7a79945bc8c362cc1082e57815dc0208d496
Dominik Tränklein <dominik@bleech.de>
2017-06-29 11:03:13 +0200

    docs(ACFComposer): minor adjustments

--- lib/ACFComposer/ACFComposer.php
+++ lib/ACFComposer/ACFComposer.php
@@ -4,13 +4,15 @@ namespace ACFComposer;
 
 class ACFComposer
 {
-	/**
-	 * Registers a local field group in Advanced Custom Fields base on a config array. Included fields, subfields, etc. can either contain another array defining them, or a string that stands for a WordPress filter. This filter will be applied and its result used as the field, subfield, etc.
-	 *
-	 * @param array $config Configuration array for the local field group.
-	 *
-	 * @return boolean Was the field group added or not.
-	 */
+    /**
+     * Registers a local field group in Advanced Custom Fields based on a config array.
+     *
+     * Included fields, subfields, etc. can either contain another array defining them, or a string that stands for a WordPress filter. This filter will be applied and its result used as the field, subfield, etc.
+     *
+     * @param array $config Configuration array for the local field group.
+     *
+     * @return boolean Was the field group added or not.
+     */
     public static function registerFieldGroup($config)
     {
         $fieldGroup = ResolveConfig::forFieldGroup($config);
